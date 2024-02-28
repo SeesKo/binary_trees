e70e4c4febc0a817c389948c2337ddb48103ce1a
Siskosee <seeskosee@seeskoseesko.com>
2024-02-28 06:59:48 -0800

    header file

--- binary_trees.h
+++ binary_trees.h
@@ -21,12 +21,18 @@ typedef struct binary_tree_s
 	struct binary_tree_s *right;
 } binary_tree_t;
 
+typedef struct binary_tree_s binary_tree_t;
+
+/* For BST */
 typedef struct binary_tree_s bst_t;
+/* For AVL */
 typedef struct binary_tree_s avl_t;
+/* For Max Binary Heap */
 typedef struct binary_tree_s heap_t;
 
 void binary_tree_print(const binary_tree_t *);
 
+/* MANDATORY */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -49,20 +55,21 @@ size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+
+/* OPTIONAL */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+
 int binary_tree_is_bst(const binary_tree_t *tree);
 bst_t *bst_insert(bst_t **tree, int value);
 bst_t *array_to_bst(int *array, size_t size);
 bst_t *bst_search(const bst_t *tree, int value);
 bst_t *bst_remove(bst_t *root, int value);
-int binary_tree_is_avl(const binary_tree_t *tree);
-avl_t *avl_insert(avl_t **tree, int value);
-avl_t *array_to_avl(int *array, size_t size);
+
 int binary_tree_is_avl(const binary_tree_t *tree);
 int is_balanced(const binary_tree_t *tree, int min, int max);
 int binary_tree_is_bst(const binary_tree_t *tree);
@@ -71,11 +78,9 @@ int is_bst_helper(const binary_tree_t *tree, const binary_tree_t *min,
 size_t binary_tree_height(const binary_tree_t *tree);
 
 avl_t *avl_insert(avl_t **tree, int value);
-avl_t *avl_insert_recursive(avl_t *tree, int value);
-avl_t *balance_tree(avl_t *tree);
-int avl_height(const avl_t *tree);
-int avl_balance(const avl_t *tree);
-int max(int a, int b);
+avl_t *array_to_avl(int *array, size_t size);
+avl_t *avl_remove(avl_t *root, int value);
+avl_t *sorted_array_to_avl(int *array, size_t size);
 
 int binary_tree_is_heap(const binary_tree_t *tree);
 heap_t *heap_insert(heap_t **root, int value);
