71a11680a01a097bef8487d0162337a569be6e41
Siskosee <seeskosee@seeskoseesko.com>
2024-02-29 12:59:31 -0800

    max binary heap to sorted array

--- 134-heap_to_sorted_array.c
+++ 134-heap_to_sorted_array.c
@@ -3,46 +3,53 @@
 int get_tree_size(heap_t *root);
 
 /**
- * heap_to_sorted_array - Converts a Binary Max Heap to a sorted array.
+ * heap_to_sorted_array - Converts a Binary Max Heap
+ * to a sorted array.
+ * @heap: Pointer to the root node of the Binary Max Heap.
+ * @size: Pointer to the variable that will store the size
+ * of the array.
+ * Return: Pointer to the sorted array, or NULL on failure.
  */
 int *heap_to_sorted_array(heap_t *heap, size_t *size)
 {
 	size_t heap_size, i;
 	int *sorted_array;
 
-    if (!heap || !size)
-        return (NULL);
+	if (!heap || !size)
+		return (NULL);
 
-    /* Getting the size of the heap */
-    heap_size = get_tree_size(heap);
+	/* Getting the size of the heap */
+	heap_size = get_tree_size(heap);
 
-    /* Allocate memory for the array */
-    sorted_array = malloc(sizeof(int) * heap_size);
-    if (!sorted_array)
-        return (NULL);
+	/* Allocate memory for the array */
+	sorted_array = malloc(sizeof(int) * heap_size);
+	if (!sorted_array)
+		return (NULL);
 
-    /* Extract elements from the heap and store in the array */
-    for (i = 0; i < heap_size; ++i)
-        sorted_array[i] = heap_extract(&heap);
+	/* Extract elements from the heap and store in the array */
+	for (i = 0; i < heap_size; ++i)
+		sorted_array[i] = heap_extract(&heap);
 
-    /* Update the size variable */
-    *size = heap_size;
+	/* Update the size variable */
+	*size = heap_size;
 
-    return (sorted_array);
+	return (sorted_array);
 }
 
 /**
  * get_tree_size - Get the size of the tree.
+ * @root: Pointer to the root node of the binary tree.
+ * Return: Size of the binary tree.
  */
 int get_tree_size(heap_t *root)
 {
-    int left_size, right_size;
+	int left_size, right_size;
 
-    if (!root)
-        return (0);
+	if (!root)
+		return (0);
 
-    left_size = get_tree_size(root->left);
-    right_size = get_tree_size(root->right);
+	left_size = get_tree_size(root->left);
+	right_size = get_tree_size(root->right);
 
-    return (1 + left_size + right_size);
+	return (1 + left_size + right_size);
 }
